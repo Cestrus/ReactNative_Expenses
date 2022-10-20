685e59abb82b43d5f98d5b3f1678db101531f9e1
Aleksey Savchenko <wasxela@gmail.com>
2022-10-20 11:15:06 +0300

    fix style Input component

--- components/ManageExpense/Input/Input.tsx
+++ components/ManageExpense/Input/Input.tsx
@@ -1,15 +1,42 @@
 import React from 'react';
-import { View, Text, TextInput } from 'react-native';
+import { View, Text, TextInput, StyleSheet } from 'react-native';
+import { GlobalStyles } from '../../../constans/styles';
 
 import { IInputProps } from './Input.props';
 
 const Input: React.FC<IInputProps> = ({ label, textInputConfig }) => {
   return (
-    <View>
-      <Text>{label}</Text>
-      <TextInput {...textInputConfig} />
+    <View style={styles.container}>
+      <Text style={styles.label}>{label}</Text>
+      <TextInput
+        style={textInputConfig && textInputConfig.multiline ? [styles.input, styles.inputMultiline] : styles.input}
+        {...textInputConfig}
+      />
     </View>
   );
 };
 
 export default Input;
+
+const styles = StyleSheet.create({
+  container: {
+    marginHorizontal: 4,
+    marginVertical: 16,
+  },
+  label: {
+    fontSize: 12,
+    color: GlobalStyles.colors.primary100,
+    marginBottom: 4,
+  },
+  input: {
+    color: GlobalStyles.colors.primary700,
+    backgroundColor: GlobalStyles.colors.primary100,
+    padding: 6,
+    borderRadius: 6,
+    fontSize: 18,
+  },
+  inputMultiline: {
+    minHeight: 100,
+    textAlignVertical: 'top',
+  },
+});
